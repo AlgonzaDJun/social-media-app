c7bf2f914d1b29d6f58f919844529dd9fff16dd9
AlgonzaDJun <algonsadewangga@gmail.com>
2023-10-21 14:05:01 +0700

    feat: post card (unfinished)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,19 @@
 import Card from "./Card";
 import NavigationCard from "./NavigationCard";
 import PostFormCard from "./PostFormCard";
+import PostCard from "./PostCard";
 
 const Home = () => {
   return (
     <div className="mt-7 flex gap-2 md:gap-8 max-w-full w-full md:max-w-4xl mx-auto">
-      <div className=" md:w-1/3">
+      <div className="md:w-3/12">
         <NavigationCard />
-      </div>
+      </div>  
 
       {/* POST DAN FORM */}
-      <div className="grow">
+      <div className="grow md:w-9/12">
         <PostFormCard />
-        <Card>Story of people</Card>
+        <PostCard />
       </div>
     </div>
   );
